8e1f9a61a9ac38771b649b30538363f905c49cbf
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-10-06 14:33:24 +0200

    Display de la liste des tags dans card/new, SANS les mettre en choix. a améliorer plus tard

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   validates :question, presence: true
-  validates :answer, presence: true, length: { maximum: 140 }
+  validates :answer, length: { maximum: 140 }
   acts_as_taggable
   validates :frequence, inclusion: { in: %w(plus moyen moins), message: "%{value} n'est pa sune fréquence valide" }
   mount_uploader :picture, PictureUploader
