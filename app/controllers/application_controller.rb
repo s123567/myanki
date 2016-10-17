1426b5bfac91645ab8019c4cc1647df15e2fa209
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-10-17 15:58:35 +0200

    changed signup signin form, buttons, routes after successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def after_sign_in_path_for(resource)
+      cards_path
+    end
+
     # Overwriting the sign_out redirect path method
     def after_sign_out_path_for(user)
       root_path
