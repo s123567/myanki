049b00f438e07ac80a26955d5d604ad8a65347ef
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-10-25 12:10:25 +0200

    changed notif locations, add effects on hover, harmonize colors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+// $( document ).ready(function() {
+//   $('.index-btns').hover(
+//   function(){$(this).toggleClass('pulse');}
+//   );   
+// });
+
